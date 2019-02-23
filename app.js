7a94cc2ae80675be856014b6b58ce0af4150244f
Markus Thulin <macky_b@hotmail.com>
2019-02-23 23:38:08 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const request = require('request')
+const hltb = require('howlongtobeat');
+const hltbService = new hltb.HowLongToBeatService();
+const app = express()
+const port = 4567
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/search/:name', (req, res) => {
+	hltbService.search(req.params.name).then(result => {
+		res.json(result);
+	});
+});
+app.get('/id/:nr', (req, res) => {
+	hltbService.detail(req.params.nr).then(result => {
+		res.json(result);
+	});
+
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
