5ad0a6b53704985f7577d230211348bdd16e6bc1
Markus Thulin <macky_b@hotmail.com>
2019-02-24 21:34:29 +0100

    Added mocha and a dummy test

---
+++ tests/routes.spec.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var expect = require('chai').expect;
+var should = require('chai').should();
+
+it('should return true', function(){
+      var isValid = true
+      expect(isValid).to.be.true;
+});
+it('should return false', function(){
+      var isValid = false
+      isValid.should.equal(false);
+});
