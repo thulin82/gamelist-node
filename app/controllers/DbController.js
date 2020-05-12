ac1d3b1058f1fca258255885b3b18602cb8efb85
Markus Thulin <macky_b@hotmail.com>
2020-05-12 10:40:24 +0200

    Initial setup of db (mongoDb)

---
+++ app/controllers/DbController.js
@@ -0,0 +1,15 @@
+class DbController {
+    get(req, res) {
+        res.send('Hello World!');
+    }
+    post(req, res) {
+        res.status(204).send();
+    }
+    put(req, res) {
+        res.status(204).send();
+    }
+    delete(req, res) {
+        res.status(204).send();
+    }
+}
+module.exports = new DbController();
--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var basicController = require('./controllers/BasicController');
 var idController = require('./controllers/IdController');
 var searchController = require('./controllers/SearchController');
+var dbController = require('./controllers/DbController');
 
 router.get('/', basicController.get);
 router.post('/', basicController.post);
@@ -11,6 +12,12 @@ router.delete('/', basicController.delete);
 
 router.get('/search/:name', searchController.search);
 router.get('/one/:title', searchController.one);
+
 router.get('/id/:nr', idController.get);
+
+router.get('/id/:nr', dbController.get);
+//router.post('/', dbController.post);
+//router.put('/', dbController.put);
+router.delete('/id/:nr', dbController.delete);
 
 module.exports = router;
