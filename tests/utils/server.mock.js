3f065a0756b33602b07e5913f1b2585bb10c8070
Markus Thulin <macky_b@hotmail.com>
2020-06-25 14:09:03 +0200

    Updated path from tests

--- tests/utils/server.mock.js
+++ tests/utils/server.mock.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('../../app/server');
+var server = require('../../server');
 
 chai.use(chaiHttp);
 module.exports = chai.request(server).keepOpen();
