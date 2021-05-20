5a89ae2539525aa8bfb474051669f343553be1dc
Markus Thulin <macky_b@hotmail.com>
2021-05-20 15:24:06 +0200

    Try to get separate test results

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
